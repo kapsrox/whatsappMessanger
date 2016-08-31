5b5bdbc8a40e2191fb8a879132534773b3c4e9e9
kapsrox <kapil.chhangani@yahoo.co.in>
2016-08-31 21:21:17 +0530

    try
    tryyyy

--- wm_admin/core/connect/database.php
+++ wm_admin/core/connect/database.php
@@ -8,5 +8,5 @@ $config = array(
 );
 
 $db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'] . ';port=' . $config['port'], $config['username'], $config['password']);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
