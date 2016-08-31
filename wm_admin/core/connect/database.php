ef9589d8defd92786392806a94d38295541b546a
kapsrox <kapil.chhangani@yahoo.co.in>
2016-08-31 08:58:35 +0530

    update db config
    db config

--- wm_admin/core/connect/database.php
+++ wm_admin/core/connect/database.php
@@ -6,5 +6,5 @@ $config = array(
 	'dbname' 	=> 'wmmessanger'
 );
 
-$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
+$db = new PDO('mysql:host=' . getenv('OPENSHIFT_MYSQL_DB_HOST') . ';dbname=' . getenv('OPENSHIFT_APP_NAME'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
