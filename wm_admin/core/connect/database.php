b2510726c357c5bae8e81f90225eaaba823e7dc4
kapsrox <kapil.chhangani@yahoo.co.in>
2016-08-31 09:02:28 +0530

    openshift
    test

--- wm_admin/core/connect/database.php
+++ wm_admin/core/connect/database.php
@@ -1,10 +1,10 @@
 <?php 
 $config = array(
-	'host'		=> '127.12.22.130',
-	'username' 	=> 'adminYxdzbhQ',
-	'password' 	=> 'UzNBKVPG-LCL',
+	'host'		=> getenv('OPENSHIFT_MYSQL_DB_HOST'),
+	'username' 	=> getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+	'password' 	=> getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
 	'dbname' 	=> 'wmmessanger'
 );
 
-$db = new PDO('mysql:host=' . getenv('OPENSHIFT_MYSQL_DB_HOST') . ';dbname=' . getenv('OPENSHIFT_APP_NAME'), getenv('OPENSHIFT_MYSQL_DB_USERNAME'), getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
